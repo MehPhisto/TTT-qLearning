7763d899a6dc44b6c0bd919c1c9f5ba396108f75
Cedric BOLLARO <cedric.bollaro@hotmail.fr>
2019-12-18 21:50:45 +0100

    First one*

---
+++ morpionKeyces.py
@@ -0,0 +1,224 @@
+import math
+from copy import deepcopy
+import logging
+import random
+import copy
+
+
+class Player:
+    """ Skeleton for human and agent player """
+
+    def play(self, *board):
+        pass
+
+    def receiveReward(self, reward):
+        pass
+
+
+class HumanPlayer(Player):
+    def play(self, board):
+        try:
+            while True:
+                print("Entrer un entier entre 0 et 2 pour x")
+                x = int(input())
+                print("Entrer un entier entre 0 et 2 pour y")
+                y = int(input())
+                # no cheat
+                if (x > -1 and x < 3) and (y > -1 and y < 3) and board[x][y] == " ":
+                    return x, y
+        except:
+            print('Saisir de bonne valeur')
+            return self.play(board)
+
+
+class Agent(Player):
+    def __init__(self, learning_factor, refresh_factor, file_name):
+
+        self.filename = file_name
+
+        try:
+            save_file = open(self.filename, 'r')
+            data_loaded = save_file.read()
+
+            if len(data_loaded) == 0:
+                data_loaded = {}
+
+            save_file.close()
+
+        except:
+            data_loaded = {}
+
+        self.data_loaded = data_loaded
+
+        self.learning_factor = learning_factor
+        self.refresh_factor = refresh_factor
+
+        # self.learning_factor = 0.5 if self.learning_factor >= 1 or self.learning_factor <= 0 else print("potato")
+        # self.refresh_factor = 0.5 if self.refresh_factor >= 1 or self.refresh_factor  <= 0 else print("potato")
+
+    def play(self, board, current_player):
+        available_pos = self.findAvailablePositions(board)
+        current_best = (None, None)
+        current_int = 0
+
+        for i in range(0, len(available_pos)):
+            tmp_board = copy.deepcopy(board)
+            tmp_board[available_pos[i][0]][available_pos[i][1]] = current_player
+
+            if str(tmp_board) in self.data_loaded:
+                if self.data_loaded[str(tmp_board)] > current_int:
+                    current_int = self.data_loaded[str(tmp_board)]
+                    current_best = ([available_pos[i][0]], [available_pos[i][1]])
+
+        if current_best == (None, None):
+            random_value = random.randint(0, len(available_pos)-1)
+            return available_pos[random_value]
+        else:
+            return current_best
+
+    def receiveReward(self, reward):
+
+        dataToAppend = ''
+
+        self.filename = "save.txt"
+
+        save_file = open(self.filename, 'w+')
+
+        save_file.write(dataToAppend)
+
+        save_file.close()
+
+    def findAvailablePositions(self, board):
+        available_positions = []
+        for i in range(0, len(board)):
+            for j in range(0, len(board[i])):
+                if board[i][j] == ' ':
+                    available_positions.append((i, j))
+
+        return available_positions
+
+
+class Environment:
+    def __init__(self, player1, player2, withHuman=False):
+        self.board = []
+        self.player1 = player1
+        self.player2 = player2
+        self.canPlay = True
+        self.playerWinner = ''
+        self.withHuman = withHuman
+
+    def initGame(self):
+        """  Defines a bard with 9 empty case, ie a space
+            and indicates if a player can play
+        """
+        self.board = [[' ' for i in range(3)] for j in range(3)]
+        self.canPlay = True
+        self.playerWinner = ''
+
+    def launchGame(self):
+        """ Alternate the move between the two player """
+        self.initGame()
+        currentPlayer = 'X'
+        continueGame = True
+        while continueGame:
+            # print for human
+            if self.withHuman:
+                print(self)
+            if currentPlayer == 'X':
+                x, y = self.player1.play(self.board, currentPlayer)
+                self.board[x][y] = currentPlayer
+                currentPlayer = 'O'
+            else:
+                x, y = self.player2.play(self.board, currentPlayer)
+                self.board[x][y] = currentPlayer
+                currentPlayer = 'X'
+            # TO DO give the new state to agents
+            if self.winner() or not self.playerCanPlay():
+                if self.withHuman:
+                    print(self)
+                logging.info('*** End game *** \n' + self.__str__())
+                if self.playerWinner != '':
+                    if self.withHuman:
+                        print(self.playerWinner, 'win')
+                    logging.info('winner is ' + self.playerWinner)
+                else:
+                    if self.withHuman:
+                        print('null game')
+                    logging.info('null game')
+                continueGame = False
+                self.giveRewards()
+
+    def winner(self):
+        """ Returns True if a winner exists or False
+        """
+        # check the rows
+        for i in range(3):
+            if self.board[i][0] == self.board[i][1] and self.board[i][0] == self.board[i][2] and self.board[i][
+                0] != " ":
+                self.playerWinner = self.board[i][0]
+                return True
+        # check the colunms
+        for i in range(3):
+            if self.board[0][i] == self.board[1][i] and self.board[0][i] == self.board[2][i] and self.board[0][
+                i] != " ":
+                self.playerWinner = self.board[0][i]
+                return True
+        # check diagonales
+        if self.board[0][0] == self.board[1][1] and self.board[0][0] == self.board[2][2] and self.board[0][0] != " ":
+            self.playerWinner = self.board[i][0]
+            return True
+        if self.board[0][2] == self.board[1][1] and self.board[0][2] == self.board[2][0] and self.board[0][2] != " ":
+            self.playerWinner = self.board[i][0]
+            return True
+        return False
+
+    def playerCanPlay(self):
+        """ Use to determine null game """
+        for row in self.board:
+            if row.count(' ') > 0:
+                return True
+        return False
+
+    def giveRewards(self):
+        print(self.playerWinner)
+        if self.playerWinner.upper() == 'X':
+            self.player1.receiveRewards(1)
+            self.player2.receiveRewards(0)
+        elif self.playerWinner.upper() == 'O':
+            self.player2.receiveRewards(1)
+            self.player1.receiveRewards(0)
+        else:
+            self.player1.receiveRewards(0.5)
+            self.player2.receiveRewards(0.5)
+
+        print(self.__str__())
+
+    def __str__(self):
+        s = ''
+        for i in range(3):
+            s += "|"
+            for j in range(3):
+                s += self.board[i][j] + "|"
+            s += '\n'
+        return s
+
+
+if __name__ == '__main__':
+    print('*** TRAININ PERIOD ***')
+    # # to log each action of agent
+    # logging.basicConfig(self.filename='game.log', level=logging.DEBUG)
+
+    agent1 = Agent(0.92, 0.42, "fileSave1")
+    agent2 = Agent(0.66, 0.33, "fileSave2")
+    game = Environment(agent1, agent2)
+    for i in range(0, 200):
+        print("Turn {} of training".format(i))
+        game.launchGame()
+    # play with the agent
+    # print(" Let's go now !!! ")
+    # continuePlay = 'y'
+    # human = HumanPlayer()
+    # game = Environment(agent1, human, True)
+    # while continuePlay == 'y':
+    #     game.launchGame()
+    #     continuePlay = input('Again?(y/n)')
